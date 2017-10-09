0fc8c748ac6904303c3bbfb688081a6dec78c8d3
Arkadyuti Sarkar <arkadooti.sarkar@gmail.com>
2017-10-09 20:28:41 +0530

    Footer component updated

--- app/components/common/Footer/index.js
+++ app/components/common/Footer/index.js
@@ -32,8 +32,9 @@ class Footer extends React.Component { // eslint-disable-line react/prefer-state
           <div className="col-xs-12 col-sm-12 col-md-12 footer-first-container">
             <div className="row">
               <div className="col-xs-12 col-sm-12 col-md-12">
-                <h2>BEST CASHBACK, BEST DEALS & LATEST COUPONS</h2>
-                <h3>Save at 500+ online websites like Amazon, Paytm, Flipkart, Snapdeal & more</h3>
+                <img className="footer-logo" src="/img/logo_footer3.png" alt="CropmyBill footer logo"/>
+                <h2 className="footer-heading">BEST CASHBACK, BEST DEALS & LATEST COUPONS</h2>
+                <h3 className="footer-subheading">Save at 500+ online websites like Amazon, Paytm, Flipkart, Snapdeal & more</h3>
               </div>
               <div className="col-xs-12 col-sm-12 col-md-12 footer-list-container">
                 {this.renderFooterList()}
@@ -54,13 +55,13 @@ class Footer extends React.Component { // eslint-disable-line react/prefer-state
                 </ul>
               </div>
               <div className="col-xs-12 col-sm-2 col-md-2">
-                Terms and conditions
+                <a href="#terms">Terms & Conditions</a>
               </div>
               <div className="col-xs-12 col-sm-2 col-md-2">
-                <span className="facebook">facebook</span>
-                <span className="facebook">twitter</span>
+                <span className="social facebook">facebook</span>
+                <span className="social twitter">twitter</span>
               </div>
-              <div className="col-xs-12 col-sm-6 col-md-6">care</div>
+              <div className="col-xs-12 col-sm-3 col-md-3 col-sm-offset-2 col-md-offset-2 care-email">Contact Us <a href="mailto:care@cropmybill.com">care@cropmybill.com</a></div>
             </div>
           </div>
         </div>
